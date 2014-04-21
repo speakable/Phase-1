c85dc43e5d165f2e5b596f89014f4d7d2260a1a3
Elliott Hindman <Elliott Hindman@Laptop2>
2014-04-21 09:55:15 +0900

    removed spacing between cells in robot game

--- EnglishSite/Speakable/assets/libraries/game_engine/game_finished.js
+++ EnglishSite/Speakable/assets/libraries/game_engine/game_finished.js
@@ -2,7 +2,7 @@ function displayFinish(container, score, onretry) {
 	$(container).empty();
 	$(container).append("<div class='finishprompt'></div>");
 	$('.finishprompt').append("<div class='resultWindow'><img id='resultGraphic' src='assets/img/share_panel.png'></div>");
-	$('.resultWindow').append("<div id='scoreReportdiv'<p>" + score + " points</p></div>");	
+	$('.resultWindow').append("<div id='scoreReportdiv'<p>" + score + "</p></div>");	
 	$('.resultWindow').append("<div id='restartGamediv'onClick=" + onretry + "()></div>");
 	$('.resultWindow').append("<a href='https://twitter.com/home?status=www.speakable.jp'><div id='twitterLinkdiv'></div></a>");
 	$('.resultWindow').append("<a href='https://www.facebook.com/sharer/sharer.php?u=www.speakable.jp'><div id='facebookLinkdiv'></div></a>");		
